1f869c728746874903044f3fcc91f96c7f5894e2
lukas-123 <lukas.steve@web.de>
2016-04-11 00:00:22 +0200

    Fix loop logic

--- desktop.py
+++ desktop.py
@@ -8,9 +8,6 @@ def isValidFilepath(filepath):
     except OSError:
         return False
 
-
-app_directory = '/usr/share/applications/'
-
 desktop_entry = "[Desktop Entry]\n"
 desktop_entry += "Type=Application\n"
 
@@ -23,10 +20,10 @@ while(not isValidFilepath(app_exec)):
 desktop_entry += "Exec=" + app_exec + "\n"
 
 app_icon = raw_input("Path to icon (empty for no icon):\n")
-while(not app_icon == "" or not isValidFilepath(app_icon)):
+while(not app_icon == '' and not isValidFilepath(app_icon)):
     app_icon = raw_input("File does not exist. Try again:\n")
-if(not (app_icon == "")):
-    desktop_entry += "Icon=" + app_icon + "\n"
+if(not (app_icon == '')):
+    desktop_entry += 'Icon=' + app_icon + "\n"
 
 filename = app_name.replace(' ','') + '.desktop'
 with open(filename, 'w') as desktop_file:
