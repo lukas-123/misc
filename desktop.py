06f9a69b424a1e379199977b64ea28847e259e1a
lukas-123 <lukas.steve@web.de>
2016-04-10 23:39:50 +0200

    Initial commit

---
+++ desktop.py
@@ -0,0 +1,36 @@
+from os import stat
+import subprocess
+import pexpect
+
+def isValidFilepath(filepath):
+    try:
+        stat(app_exec)
+        return True
+    except OSError:
+        return False
+
+
+app_directory = '/usr/share/applications/'
+
+desktop_entry = "[Desktop Entry]\n"
+desktop_entry += "Type=Application\n"
+
+app_name = raw_input("Name of the application:\n")
+desktop_entry += "Name=" + app_name + "\n"
+
+app_exec = raw_input("Path to application:\n")
+while(not isValidFilepath(app_exec)):
+    app_exec = raw_input("File does not exist. Try again:\n")
+desktop_entry += "Exec=" + app_exec + "\n"
+
+app_icon = raw_input("Path to icon (empty for no icon):\n")
+while(not app_icon == "" or not isValidFilepath(app_icon)):
+    app_icon = raw_input("File does not exist. Try again:\n")
+if(not (app_icon == "")):
+    desktop_entry += "Icon=" + app_icon + "\n"
+
+filename = app_name.replace(' ','') + '.desktop'
+with open(filename, 'w') as desktop_file:
+    desktop_file.write(desktop_entry)
+    desktop_file.close()
+
